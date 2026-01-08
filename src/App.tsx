23c13e4d9ca6ea3c7de48512a8fe95dcafe1295f
jji-102 <jnon.th102@gmail.com>
2026-01-08 15:53:11 +0700

    Initial dashboard commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PanelistDashboard from './PanelistDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <PanelistDashboard />
+    </div>
+  );
+}
+
+export default App;
